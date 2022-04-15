56fb6c96132b16930dcf1d8dbc56ffdca12d59ce
Moritz Ulrich <moritz@tarn-vedra.de>
2022-04-15 20:16:47 +0200

    WIP WebWorker implementation

---
+++ worker.js
@@ -0,0 +1,22 @@
+// The worker has its own scope and no direct access to functions/objects of the
+// global scope. We import the generated JS file to make `wasm_bindgen`
+// available which we need to initialize our WASM code.
+importScripts('./pkg/fit_utils.js');
+
+console.log('Initializing worker')
+
+// In the worker, we have a different struct that we want to use as in
+// `index.js`.
+const { WasmDecoder } = wasm_bindgen;
+
+async function init_wasm_in_worker() {
+  // Load the wasm file by awaiting the Promise returned by `wasm_bindgen`.
+  await wasm_bindgen('./pkg/fit_utils_bg.wasm');
+
+  let decoder = WasmDecoder.new();
+  self.onmessage = async event => {
+    decoder.process(event.data, msg => self.postMessage(msg));
+  };
+};
+
+init_wasm_in_worker();

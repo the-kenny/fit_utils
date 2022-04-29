b37a6bb2655a460f46f82822456c971db36cf979
Moritz Ulrich <moritz@tarn-vedra.de>
2022-04-29 15:41:48 +0200

    fit: Implement streamed and support stdin as input

--- src/bin/fit.rs
+++ src/bin/fit.rs
@@ -1,15 +1,21 @@
-use std::{fs::File, io::BufReader, path::PathBuf};
+use std::{
+    fs::File,
+    io::{stdin, BufReader, Read},
+    path::PathBuf,
+};
 
 use clap::Parser;
-use log::debug;
+use log::{debug, error};
 
-use fit_utils::inflate;
+use fit_utils::{inflate, streaming_fit_decoder::StreamingFitDecoder};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(long)]
     wgs84: bool,
+    #[clap(long)]
+    skip_undecodeable: bool,
     fit_files: Vec<PathBuf>,
 }
 
@@ -17,21 +23,46 @@ fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
     debug!("Args: {args:?}");
 
-    for file in args.fit_files {
-        let file = BufReader::new(File::open(file)?);
-        let mut reader = inflate(file)?;
+    if args.fit_files.is_empty() {
+        let stdin = stdin();
+        let input = stdin.lock();
 
-        let mut fit = fitparser::from_reader(&mut reader).expect("Fit parsing failed");
+        process_stream(&args, input)
+    } else {
+        for file in &args.fit_files {
+            let input = BufReader::new(File::open(file)?);
+            let input = inflate(input)?;
+            process_stream(&args, input)?
+        }
+        Ok(())
+    }
+}
 
+fn process_stream<R: Read>(args: &Args, input: R) -> Result<(), anyhow::Error> {
+    let fit_iter = StreamingFitDecoder::new(input).into_iterator();
+
+    // Handle errors
+    let fit_iter = fit_iter.flat_map(|r| match r {
+        Ok(r) => Some(r),
+        Err(e) if args.skip_undecodeable => panic!("Failed to decode fit message: {e}"),
+        Err(e) => {
+            error!("Failed to decode fit message {e}");
+            None
+        }
+    });
+
+    // WGS84 Pipeline
+    let fit_iter = fit_iter.map(|mut r| {
         if args.wgs84 {
-            for r in &mut fit {
-                fit_utils::normalize_wgs84(r);
-            }
+            fit_utils::normalize_wgs84(&mut r);
+            r
+        } else {
+            r
         }
+    });
 
-        for record in &fit {
-            println!("{}", fit_utils::to_json(record)?);
-        }
+    for record in fit_iter {
+        println!("{}", fit_utils::to_json(&record)?);
     }
 
     Ok(())
